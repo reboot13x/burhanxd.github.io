3321c6285fc5fc0154e15d55e0c90592f719e472
Burhan Chhotaudepur <65703940+burhanxd@users.noreply.github.com>
2020-12-04 19:01:59 +0530

    Files

---
+++ index.js
@@ -0,0 +1,15 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+});
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
